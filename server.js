6c9e5289fbcd876232e404b63be5948eb34d2ee6
pepepigman <john_winther@protonmail.ch>
2024-06-02 19:59:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { Keypair } = require('@solana/web3.js');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
