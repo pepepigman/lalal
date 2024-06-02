b92c15fbd9a5dc2824b4af243855eb4e49f669d5
pepepigman <john_winther@protonmail.ch>
2024-06-02 20:21:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Update with your Ngrok URL
-const serverUrl = 'https://lalal-eight.vercel.app/';
+const serverUrl = 'https://lalal-gules.vercel.app/';
 
 // Function to create a wallet
 async function createWallet() {
